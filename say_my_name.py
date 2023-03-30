6c1ead0c74f51b54f11d0e0e9125c20d39b0c147
Jaelli <jadelilley1@outlook.com>
2023-03-30 20:39:45 +0100

    first commit

---
+++ say_my_name.py
@@ -0,0 +1,11 @@
+
+firstName = "Jade"
+lastName = "Lilley"
+
+print(firstName, lastName)
+
+fname = input("Enter first name: ")
+sname = input("Enter surname: ")
+age = int(input("Enter your age: "))
+print(type(age))
+print(fname, sname, age*10)
